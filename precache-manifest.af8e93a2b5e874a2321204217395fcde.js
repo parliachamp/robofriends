578cf75796d6e5b99011e5232ce5c22e8ed72ae1
unknown <parliachamp@gmail.com>
2019-12-06 20:48:43 +0800

    Updates

---
+++ precache-manifest.af8e93a2b5e874a2321204217395fcde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a705a07fa766f07070e68e2e09728674",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "10ca8a264ab078a444e0",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8bdb56dece9acfd07b65",
+    "url": "/robofriends/static/css/main.eb03c4bf.chunk.css"
+  },
+  {
+    "revision": "10ca8a264ab078a444e0",
+    "url": "/robofriends/static/js/2.49b130ef.chunk.js"
+  },
+  {
+    "revision": "8bdb56dece9acfd07b65",
+    "url": "/robofriends/static/js/main.41e6caa8.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
